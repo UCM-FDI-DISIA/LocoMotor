a4be111f0a3a23c8935b52c32e1ca6575470b3fc
dcasiano <dcasiano@ucm.es>
2024-02-25 16:40:37 +0100

    directorios de inclusion (lo mismo falta alguno)

---
+++ src/LocoMotor_Graphics/cpp/GraphicsManager.cpp
@@ -0,0 +1,240 @@
+//Ogre includes
+#include <OgreRoot.h>
+#include <OgreConfigFile.h>
+#include <OgreRenderWindow.h>
+#include <OgreGpuProgramManager.h>
+#include <OgreShaderGenerator.h>
+#include <OgreOverlaySystem.h>
+//SDL includes
+#include <SDL.h>
+#include <SDL_video.h>
+#include <SDL_syswm.h>
+//Std include
+#include <iostream>
+//Graphics includes
+#include "RenderScene.h"
+#include "SGTechniqueResolverListener.h"
+#include "GraphicsManager.h"
+
+using namespace LocoMotor;
+
+Graphics::GraphicsManager* Singleton<Graphics::GraphicsManager>::_instance = nullptr;
+
+Graphics::GraphicsManager::GraphicsManager() {
+	_activeScene = nullptr;
+	_mShaderGenerator = nullptr;
+	_root = nullptr;
+	_ovrSys = nullptr;
+}
+
+Graphics::GraphicsManager::~GraphicsManager() {
+	Shutdown();
+}
+
+std::string Graphics::GraphicsManager::Initialize(std::string name) {
+	try {
+		_root = new Ogre::Root();
+		_ovrSys = new Ogre::OverlaySystem();
+	}
+	catch (...) {
+		return "Error while constructing internal ogre library";
+	}
+	try {
+		_root->showConfigDialog(nullptr);
+		_root->restoreConfig();
+		_root->initialise(false);
+	}
+	catch (...) {
+		return "Error while initializing internal ogre library";
+	}
+	try {
+		InitWindow(name);
+	}
+	catch (...) {
+		return "Error while initializing window";
+	}
+	try {
+		LoadResources();
+	}
+	catch (...) {
+		return "Error while loading resources: make sure all .fontdef, .material, .particle, etc have no duplicates/are correctly written";
+	}
+	return "";
+}
+
+Graphics::RenderScene* Graphics::GraphicsManager::CreateScene(std::string name) {
+	if (_scenes.count(name) > 0) {
+		std::cerr << "ERROR: Ya hay una escena con el nombre \"" << name << "\". Elige otro nombre por favor\n";
+		return _scenes[name];
+	}
+	Graphics::RenderScene* sc;
+	Ogre::SceneManager* sM = _root->createSceneManager();
+	sM->addRenderQueueListener(_ovrSys);
+	sc = new Graphics::RenderScene(sM);
+	_scenes.insert({ name, sc });
+	if (_activeScene == nullptr) _activeScene = sc;
+	_mShaderGenerator->addSceneManager(sc->GetMan());
+	return sc;
+}
+
+Graphics::RenderScene* Graphics::GraphicsManager::GetScene(std::string name) {
+	if (_scenes.count(name) == 0)
+		return nullptr;
+
+	return _scenes[name];
+}
+
+void Graphics::GraphicsManager::Render() {
+	if (_activeScene == nullptr) return;
+	_activeScene->Render();
+	_root->renderOneFrame();
+}
+
+Ogre::RenderWindow* Graphics::GraphicsManager::GetRenderWindow() {
+	return _mWindow.render;
+}
+
+void Graphics::GraphicsManager::SetActiveScene(Graphics::RenderScene* s) {
+	_activeScene = s;
+}
+
+int Graphics::GraphicsManager::GetWindowHeight() {
+	return _mWindow.render->getHeight();
+}
+
+int Graphics::GraphicsManager::GetWindowWidth() {
+	return _mWindow.render->getWidth();
+}
+
+void Graphics::GraphicsManager::LoadResources() {
+	Ogre::ConfigFile cf;
+	cf.load("resources.cfg");
+
+	Ogre::String sec_name , type_name , arch_name;
+	Ogre::ConfigFile::SettingsBySection_ seci = cf.getSettingsBySection();
+
+	for (Ogre::ConfigFile::SettingsBySection_::const_iterator it = seci.cbegin(); it != seci.cend(); ++it) {
+		sec_name = it->first;
+		Ogre::ConfigFile::SettingsMultiMap settings = it->second;
+		Ogre::ConfigFile::SettingsMultiMap::iterator i;
+
+		for (i = settings.begin(); i != settings.end(); ++i) {
+			type_name = i->first;
+			arch_name = i->second;
+			Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+				arch_name , type_name , sec_name);
+		}
+	}
+
+	sec_name = Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME;
+	const Ogre::ResourceGroupManager::LocationList genLocs = Ogre::ResourceGroupManager::getSingleton().getResourceLocationList(sec_name);
+
+	arch_name = genLocs.front().archive->getName();
+	type_name = genLocs.front().archive->getType();
+
+	std::string mRTShaderLibPath = arch_name + "/RTShaderLib";
+	Ogre::ResourceGroupManager::getSingleton().addResourceLocation(mRTShaderLibPath + "/materials" , type_name , sec_name);
+
+	Ogre::ResourceGroupManager::getSingleton().addResourceLocation(mRTShaderLibPath + "/GLSL" , type_name , sec_name);
+
+	Ogre::MaterialManager::getSingleton().setActiveScheme(Ogre::MaterialManager::DEFAULT_SCHEME_NAME);
+
+	if (Ogre::RTShader::ShaderGenerator::initialize()) {
+		_mShaderGenerator = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+
+		if (mRTShaderLibPath.empty())
+			return;
+
+		// Create and register the material manager listener if it doesn't exist yet.
+		if (!_mMaterialMgrListener) {
+			_mMaterialMgrListener = new Graphics::SGTechniqueResolverListener(_mShaderGenerator);
+			Ogre::MaterialManager::getSingleton().addListener(_mMaterialMgrListener);
+		}
+	}
+
+	Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+}
+
+Graphics::NativeWindowPair Graphics::GraphicsManager::InitWindow(std::string name) {
+	uint32_t w , h;
+	Ogre::NameValuePairList miscParams;
+
+	Ogre::ConfigOptionMap ropts = _root->getRenderSystem()->getConfigOptions();
+
+	std::istringstream mode(ropts["Video Mode"].currentValue);
+	Ogre::String token;
+	mode >> w; // width
+	mode >> token; // 'x' as seperator between width and height
+	mode >> h; // height
+
+	miscParams["FSAA"] = ropts["FSAA"].currentValue;
+	miscParams["vsync"] = ropts["VSync"].currentValue;
+	miscParams["gamma"] = ropts["sRGB Gamma Conversion"].currentValue;
+
+	if (!SDL_WasInit(SDL_INIT_VIDEO)) {
+		if (SDL_GameControllerAddMappingsFromFile("gamecontrollerdb.txt"))
+			std::cout << "Gamecontroller Mappings Loaded" << "\n";
+		else
+			std::cout << "Gamecontroller Mappings NOT Loaded" << "\n";
+
+		SDL_InitSubSystem(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
+	}
+
+	Uint32 flags = SDL_WINDOW_RESIZABLE;
+
+	if (ropts["Full Screen"].currentValue == "Yes")  flags = SDL_WINDOW_FULLSCREEN;
+
+	_mWindow.native = SDL_CreateWindow(name.c_str() , SDL_WINDOWPOS_UNDEFINED , SDL_WINDOWPOS_UNDEFINED , w , h , flags);
+
+	SDL_SysWMinfo wmInfo;
+	SDL_VERSION(&wmInfo.version);
+	SDL_GetWindowWMInfo(_mWindow.native , &wmInfo);
+
+	miscParams["externalWindowHandle"] = Ogre::StringConverter::toString(size_t(wmInfo.info.win.window));
+
+	_mWindow.render = _root->createRenderWindow(name , w , h , false , &miscParams);
+	return _mWindow;
+}
+
+
+void Graphics::GraphicsManager::Shutdown() {
+
+	// Restore default scheme.
+	Ogre::MaterialManager::getSingleton().setActiveScheme(Ogre::MaterialManager::DEFAULT_SCHEME_NAME);
+
+	// Unregister the material manager listener.
+	if (_mMaterialMgrListener != nullptr) {
+		Ogre::MaterialManager::getSingleton().removeListener(_mMaterialMgrListener);
+		delete _mMaterialMgrListener;
+		_mMaterialMgrListener = nullptr;
+	}
+
+	// Destroy RTShader system.
+	if (_mShaderGenerator != nullptr) {
+		Ogre::RTShader::ShaderGenerator::destroy();
+		_mShaderGenerator = nullptr;
+	}
+
+	for (auto it = _scenes.begin(); it != _scenes.end(); it = _scenes.erase(it)) {
+		if (it->second == _activeScene) {
+			_activeScene = nullptr;
+		}
+		it->second->GetMan()->removeRenderQueueListener(_ovrSys);
+		_root->destroySceneManager(it->second->GetMan());
+		delete it->second;
+	}
+
+	if (_mWindow.render != nullptr) {
+		_root->destroyRenderTarget(_mWindow.render);
+		_mWindow.render = nullptr;
+	}
+
+	if (_mWindow.native != nullptr) {
+		SDL_DestroyWindow(_mWindow.native);
+		SDL_QuitSubSystem(SDL_INIT_VIDEO);
+		_mWindow.native = nullptr;
+	}
+	delete Ogre::OverlaySystem::getSingletonPtr();
+	delete _root;
+	_root = nullptr;
+}
---
+++ src/LocoMotor_Graphics/include/GraphicsManager.h
@@ -0,0 +1,99 @@
+#ifndef _GRAPHICS_MANAGER_H
+#define _GRAPHICS_MANAGER_H
+
+#include <map>
+#include <OgreMaterialManager.h>
+
+#include "Singleton.h"
+namespace Ogre {
+	class Root;
+	class RenderWindow;
+	class OverlaySystem;
+	namespace RTShader {
+		class ShaderGenerator;
+	}
+}
+
+struct SDL_Window;
+namespace LocoMotor {
+	namespace Graphics {
+		class RenderScene;
+
+		struct NativeWindowPair {
+			Ogre::RenderWindow* render = nullptr;
+			SDL_Window* native = nullptr;
+		};
+		class GraphicsManager : public LocoMotor::Singleton<GraphicsManager> {
+			friend LocoMotor::Singleton<GraphicsManager>;
+		public:
+			/// @brief Initializes the OgreManager singleton with a name for the new window
+			/// @param name Name for the window.
+			/// @return whether the initialize went well or not.
+			///	If the name is already taken, the scene with that name will be returned instead.
+			std::string Initialize(std::string name);
+
+
+			/// @brief Creates a scene, if you try to create a scene with an already used name, the method will return that scene instead.
+			/// @param name Name for the new scene.
+			/// @return The newly created scene.
+			///	If the name is already taken, the scene with that name will be returned instead.
+			Graphics::RenderScene* CreateScene(std::string name);
+
+			/// @brief Returns a created scene, searched by name in the list of scenes
+			/// @param name Name of the scene.
+			/// @return A pointer to the scene with the indicated name
+			/// If the name doesn't exist, getScene returns nullptr.
+			Graphics::RenderScene* GetScene(std::string name);
+
+			/// @brief Renders a frame.
+			void Render();
+
+			/// @brief 
+			/// Returns RenderWindow
+			/// @return Render window of Ogre
+			Ogre::RenderWindow* GetRenderWindow();
+
+			/// @brief
+			/// Sets the active scene for the manager
+			/// @param s The scene to become active
+			void SetActiveScene(Graphics::RenderScene* s);
+			/// @brief Returns the render window height
+			int GetWindowHeight();
+			/// @brief Returns the render window width
+			int GetWindowWidth();
+		protected:
+			Ogre::Root* _root;
+			NativeWindowPair _mWindow;
+
+			std::map<std::string , Graphics::RenderScene*> _scenes;
+
+			Ogre::MaterialManager::Listener* _mMaterialMgrListener = nullptr;
+
+			Graphics::RenderScene* _activeScene;
+
+			Ogre::RTShader::ShaderGenerator* _mShaderGenerator;
+
+			Ogre::OverlaySystem* _ovrSys;
+
+			/// @brief Creates a new OgreManager.
+			/// @param name The name for the window
+			GraphicsManager();
+			~GraphicsManager();
+
+			/// @brief Loads the resouces and initializes the RTShaderSytem
+			void LoadResources();
+
+			/// @brief Initializes the window
+			/// @param name The Window title
+			/// @return
+			NativeWindowPair InitWindow(std::string name);
+
+			/// @brief
+			/// Shuts down Ogre and releases all the memory related to it
+			void Shutdown();
+
+		};
+	}
+}
+
+#endif
