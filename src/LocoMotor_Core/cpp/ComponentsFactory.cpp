7a132a1424ce078b5bacc8d9246ba2c27229b0ea
jgomez18UCM <jgomez18@ucm.es>
2024-03-01 10:29:12 +0100

    Components factory

--- src/LocoMotor_Core/cpp/ComponentsFactory.cpp
+++ src/LocoMotor_Core/cpp/ComponentsFactory.cpp
@@ -1,1 +1,17 @@
 #include "ComponentsFactory.h"
+
+inline bool LocoMotor::ComponentsFactory::Init() {
+	assert(_instance == nullptr);
+	_instance = new ComponentsFactory();
+}
+
+inline void LocoMotor::ComponentsFactory::Release() {
+	assert(_instance != nullptr);
+	delete _instance;
+	_instance = nullptr;
+}
+
+inline LocoMotor::ComponentsFactory* LocoMotor::ComponentsFactory::GetInstance() {
+	assert(_instance != nullptr);
+	return _instance;
+}
--- src/LocoMotor_Core/include/ComponentsFactory.h
+++ src/LocoMotor_Core/include/ComponentsFactory.h
@@ -2,34 +2,50 @@
 #ifndef LOCOMOTOR_COMPONENTS_FACTORY
 #define LOCOMOTOR_COMPONENTS_FACTORY
 #include <string>
+#include <functional>
 #include <cassert>
 #include <map>
 
 //TODO: Programar ComponentsFactory
 namespace LocoMotor {
+	class Component;
+	typedef Component* (*CmpFactory)();
 	class ComponentsFactory {
 	public:
-		static bool Init() {
-			assert(_instance == nullptr);
-			_instance = new ComponentsFactory();
-		}
+		/// @brief Initializes the Components Factory instance
+		/// @return This method always returna true.
+		static bool Init();
+		/// @brief Relases the memory allocated by the Components Factory. This should always be called after init.
+		static void Release();
 
-		static void Release() {
-			assert(_instance != nullptr);
-			delete _instance;
-			_instance = nullptr;
-		}
-		static ComponentsFactory* GetInstance() {
-			assert(_instance != nullptr);
-			return _instance;
-		}
+		/// @return The Componenst Factory instance
+		static ComponentsFactory* GetInstance();
+
+		/// @brief Creates a component of the give typename, you can provide the type by name or by string
+		/// @tparam T Typename of the component type
+		/// @param name Name of the component type
+		/// @return A newly created Component with sufficient memory allocated for the asked type. You must cast the returned value to the desired type.
 		template <class T>
 		Component* createComponent(const std::string& name) {
-			return nullptr;
+			assert(_factories.count(name) > 0, "That component name is not registered");
+			return _factories[name]();
 		};
-		template <class T>
-		bool isUnique(const std::string& name) {
-			return unique[name];
+
+		/// @param name Name of the component type
+		/// @return true if the given component type is unique, false if not
+		inline bool isUnique(const std::string& name) {
+			assert(_unique.count(name) > 0, "That component name is not registered");
+			return _unique[name];
+		}
+
+		template <typename T>
+		void registerComponent(const std::string& name, bool unique) {
+			assert(_factories.count(name) == 0, "That component name is already registered");
+			CmpFactory fac = (CmpFactory)[]() {
+				return new T();
+			};
+			_factories.insert({ name,fac });
+			_unique.insert({ name,unique });
 		}
 	private:
 		ComponentsFactory() {
@@ -37,8 +53,8 @@ namespace LocoMotor {
 		~ComponentsFactory() {
 		};
 		static ComponentsFactory* _instance;
-		std::map<std::string, bool> unique;
+		std::map<std::string, bool> _unique;
+		std::map<std::string, CmpFactory> _factories;
 	};
 }
-
 #endif
