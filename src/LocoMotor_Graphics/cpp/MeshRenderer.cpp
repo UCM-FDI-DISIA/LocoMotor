1947a16ae1ca87ef292451ebedf0bfbdc6b190dd
GonzaloFdezMoreno <gonzaf05@ucm.es>
2024-03-07 22:39:34 +0100

    girado el cubeman

--- src/LocoMotor_Graphics/cpp/MeshRenderer.cpp
+++ src/LocoMotor_Graphics/cpp/MeshRenderer.cpp
@@ -54,6 +54,7 @@ void LocoMotor::MeshRenderer::setMaterial(std::string mat) {
 		//std::cout << _mesh->getParentNode()->getPosition().x << _mesh->getParentNode()->getPosition().y << _mesh->getParentNode()->getPosition().z << std::endl;
 		//_node->Translate(0, 0, -100);
 		_node->Scale(10, 10, 10);
+		_node->Rotate(0, 45, 45);
 		std::cout << _node->GetPosition_X()<<" " << _node->GetPosition_Y() <<" "<< _node->GetPosition_Z() << std::endl;
 	}
 }
