ab8fbd1b32db6a89f66625b4f2bfa16e92b3573a
NachoDelCastillo <82290483+NachoDelCastillo@users.noreply.github.com>
2024-03-12 14:25:22 +0100

    Enumeradores de los inputs creados en LMInputs

--- projects/Locomotor_Input/Locomotor_Input/Locomotor_Input.cpp
+++
@@ -1,9 +0,0 @@
-// Locomotor_Input.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-#include <iostream>
-#include <>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
