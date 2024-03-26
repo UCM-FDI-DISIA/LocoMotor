1181aeae95a994232f756127f9400123dbecee25
GonzaloFdezMoreno <gonzaf05@ucm.es>
2024-03-26 09:58:52 +0100

    carga de DLLs

--- src/LocoMotor_Main/cpp/main.cpp
+++ src/LocoMotor_Main/cpp/main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 #include "Engine.h"
 
+#include "windows.h"
+
 // Para que no salga la consola hay que cambiar una propiedad en vinculador->sistema, y utilizar la funcion main de windows (WinMain)
 // Como solo queremos que pase en release, hacemos este #ifdef
 #ifdef _DEBUG
 #include "CheckML.h"
 #define LocoMotor_Main	main
-#define Main_Args
+#define Main_Args       //HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow
 #else
-#include "windows.h"
+
 #define LocoMotor_Main	WinMain
 #define Main_Args		HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow
 #endif
+
+// typedef de los metodos que vamos a encontrar en la dll del Juego
+typedef const char* (CALLBACK* InitJuegoFunc)(LocoMotor::Engine* m);
 
 int LocoMotor_Main(Main_Args) {
 
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+			
 #endif
+
+	bool dllLoaded = false;
 
 	LocoMotor::Engine* i = new LocoMotor::Engine();
 
@@ -26,6 +34,41 @@ int LocoMotor_Main(Main_Args) {
 		std::cerr << "\033[1;31m" << "Algo no se inicializó correctamente" << "\033[0m" << std::endl;
 		return -1;
 	}
+
+
+#pragma region Explicit dll loading
+	LPCWSTR dllName;
+	HINSTANCE g6Game;
+	InitJuegoFunc initJuego;
+
+#ifdef _DEBUG
+	dllName = L"g6Game_d";
+#else
+	dllName = L"g6Game";
+#endif // _DEBUG
+
+	g6Game = LoadLibrary(dllName);
+	if (g6Game != NULL) {
+
+		// Buscamos la funcion, notese que no hace falta hacer lo de Wide Char
+		LPCSTR functionName = LPCSTR("InitJuego");
+		initJuego = (InitJuegoFunc) GetProcAddress(g6Game, functionName);
+
+		if (initJuego != NULL) {
+			// La ejecutamoss
+			auto result = initJuego(i);
+			std::cout << result << std::endl;
+		}
+		else {
+			std::cerr << "DLL EXPLICIT LOADING ERROR: '" << functionName << "' function couldn't be executed" << std::endl;
+		}
+
+		dllLoaded = true;
+	}
+	else {
+		std::cerr << "DLL EXPLICIT LOADING ERROR: '" << dllName << "' wasn't found" << std::endl;
+	}
+#pragma endregion
 
 	//Esto se llamaría desde el código del juego
 	if (!i->StartGameWindow("hola ventana de ogre")) {
@@ -69,6 +112,9 @@ int LocoMotor_Main(Main_Args) {
 		<< "\033[1;36m" << "A" << "\033[0m" << std::endl;
 
 	delete i;
+	if (dllLoaded) {
+		FreeLibrary(g6Game);
+	}
 
 	return 0;
 }
